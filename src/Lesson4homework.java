7214d913ea2c56e2d6fbad4dcbed7b0dfb014024
Serhii933 <Subota933@gmail.com>
2023-10-13 19:16:00 +0300

    add corecction system out println

--- src/Lesson4homework.java
+++ src/Lesson4homework.java
@@ -1,5 +1,7 @@
 public class Lesson4homework {
     public static void main (String[] args){
-        System.out.println("Привіт Світ мене звати Сергій ");
+        System.out.println("Привіт привіт  Світ мене звати Сергій ");
+
+
     }
 }
